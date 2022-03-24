c702011a4fad7a3a8ebc1af1d81b68bf8249cfc7
andrey <noise.martz@gmail.com>
2022-03-24 17:01:09 +0300

    map with walls

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,26 @@
 #include <ncurses.h>
 #include <fstream>
 #include <iomanip>
+#include <vector>
 
 int main() {
+    std::vector<const wchar_t *> map{
+        L"╔═════════════════════╗\n",
+        L"║.....................║\n",
+        L"║.....................║\n",
+        L"║.....................║\n",
+        L"║.....................║\n",
+        L"║..............M...┌─┐║\n",
+        L"║......M...........┃$│║\n",
+        L"║..................└─┘║\n",
+        L"║.....................╚═══════════════╕\n",
+        L"║.....................................┇\n",
+        L"║.....................╔═══════════════╛\n",
+        L"║.....................║\n",
+        L"║.....................║\n",
+        L"╚═════════════════════╝\n"
+    };
+
     setlocale(LC_CTYPE, "");
 
     std::ofstream log ("../log.txt", std::ios_base::app | std::ios_base::ate | std::ios_base::out);
@@ -11,19 +29,27 @@ int main() {
         std::cout << "unable to open log file." << std::endl;
         return 0;
     }
+    setlocale(LC_CTYPE, "");
     initscr();
     noecho();
-    start_color();
+    keypad(stdscr, TRUE);
     curs_set(0);
-    keypad(stdscr, true);
+	start_color();
+    use_default_colors();
 
     int row, col;
     getmaxyx(stdscr, row, col);
 
     int x = 1, y = 1;
 
+    log << map[1][1] << " " << '.' << std::endl;
+
     while (true) {
         clear();
+
+        for (auto i = 0; i < map.size(); ++i) {
+            mvaddwstr(i, 0, map[i]);
+        }
 
         chtype c = '@' | A_BLINK;
         mvaddch(y, x, c);
@@ -31,16 +57,37 @@ int main() {
         refresh();
         chtype in = getch();
         in = toupper(in);
+        bool flag = false;
 
-        if (in == KEY_UP || in == 'W') {
-            --y;
-        } else if (in == KEY_DOWN || in == 'S') {
-            ++y;
-        } else if (in == KEY_RIGHT || in == 'D') {
-            ++x;
-        } else if (in == KEY_LEFT || in == 'A') {
-            --x;
-        } else {
+        switch (in) {
+            case KEY_UP:
+            case 'W':
+                if (map[y - 1][x] == '.') {
+                    --y;
+                }
+            break;
+            case KEY_DOWN:
+            case 'S':
+                if (map[y + 1][x] == '.') {
+                    ++y;
+                }
+            break;
+            case KEY_RIGHT:
+            case 'D':
+                if (map[y][x + 1] == '.') {
+                    ++x;
+                }
+            break;
+            case KEY_LEFT:
+            case 'A':
+                if (map[y][x - 1] == '.') {
+                    --x;
+                }
+            break;
+            default:
+            flag = true;
+        }
+        if (flag) {
             auto t = std::time(nullptr);
             auto *tm = localtime(&t);
 
@@ -48,8 +95,10 @@ int main() {
 
             break;
         }
+
     }
 
+    clear();
     log.close();
     endwin();
 }
